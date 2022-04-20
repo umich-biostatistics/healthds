4aa8c42589f56957b74c08674ca8bb084e4c6c4c
Michael Kleinsasser <mk@Michaels-MacBook-Pro-3.local>
2022-04-20 13:19:09 -0400

    phil last commit

---
+++ R/growth_data.R
@@ -0,0 +1,15 @@
+#' Age, weight, and height of two newborns
+#'
+#' A made-up dataset containing an id, age, weight, and height of two newborns
+#' followed monthly for the first three months after birth. These data are
+#' used during the videos to demonstrate data wrangling ideas.
+#'
+#' @format A data frame with 8 rows and 4 variables:
+#' \describe{
+#'   \item{id}{three-number subject ID}
+#'   \item{months}{age in months at visit}
+#'   \item{weight_kg}{person's weight in kilograms}
+#'   \item{height_cm}{person's height in centimeters}
+#' }
+#'
+"growth_data"
